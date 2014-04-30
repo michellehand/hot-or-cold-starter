e081784d647c913c08f3b4a81a468ccdce135df0
michellehand <michellehand@outlook.com>
2014-04-30 12:49:04 -0500

    added feedback

--- js/app.js
+++ js/app.js
@@ -1,38 +1,50 @@
+/* Notes: got it to give feedback on the first guess, //
+but not from mulitple guess. button doesnt respond */
+
+
 
 $(document).ready(function(){
-	 newGame();
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
 
-  	});
+	newGame();
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+  /*--- Display information modal box ---*/
+  $(".what").click(function(){
+    $(".overlay").fadeIn(1000);
+  });
+
+  /*--- Hide information modal box ---*/
+  $("a.close").click(function(){
+    $(".overlay").fadeOut(1000);
+  });
+
+ 
+  /* new game */
+  $('.new').click(function(){
+    newGame();
+  });
+
+  /*guess button*/
+  $('#guessButton').click(function() {
+    event.preventDefault();
+    validate($('#userGuess').val());
+  });
 
 });
-/* new game */
-$('.new').click(function() {
-  newGame();
-  console.log(secretNumber);
-});
+
 
 /*--Variables--*/
 
 var secretNumber = randomNumber();
-var guessNum = +$('#userGuess').val();
 var guessCount = 0;
 
 
 /*-- Functions--*/
-
 function newGame() {
     $('#count').text('0');
     $('#feedback').text('Make your Guess!');
-    randomNumber();
+    secretNumber = randomNumber();
   }
+
 
 /* Random number */
 function randomNumber() {
@@ -43,8 +55,30 @@ function resetGuess() {
   $('#guessList').find("li").remove();
 }
 
-function feedback() {
-  if (isNan(guessNum) || guessNum > 100 || guessNum < 1) {
+function validate(guessNum) {
+  if (isNaN(guessNum) || guessNum > 100 || guessNum < 1) {
+    alert(guessNum);
     $("#feedback").text("Enter a number between 1 and 100");
-  };
-	}
+  } else {
+    feedback(guessNum);
+  }
+}
+
+function feedback(guessNum) {
+  var relativeNum = Math.abs(guessNum - secretNumber);
+  if (relativeNum === secretNumber) {
+    $("#feedback").text("You Guessed Correctly! Yay!");
+  } else if (relativeNum <= 10) {
+    $("#feedback").text("HOT");
+  } else if (relativeNum <= 25) {
+    $("#feedback").text("Warm");
+  } else if (relativeNum <= 50) {
+    $("#feedback").text("Cold");
+  } else {
+    $("#feedback").text("Brrrrrr!");
+  }
+}
+
+
+
+
